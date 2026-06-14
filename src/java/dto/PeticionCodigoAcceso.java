b859f292d996d391d2de3859ceb8f79c454b7dda
Izekki <95318835+Izekki@users.noreply.github.com>
2026-06-14 17:43:47 -0600

    feat: add endpoint to update access code and create PeticionCodigoAcceso DTO

---
+++ src/java/dto/PeticionCodigoAcceso.java
@@ -0,0 +1,20 @@
+package dto;
+
+public class PeticionCodigoAcceso {
+    private String codigoAcceso;
+
+    public PeticionCodigoAcceso() {
+    }
+
+    public PeticionCodigoAcceso(String codigoAcceso) {
+        this.codigoAcceso = codigoAcceso;
+    }
+
+    public String getCodigoAcceso() {
+        return codigoAcceso;
+    }
+
+    public void setCodigoAcceso(String codigoAcceso) {
+        this.codigoAcceso = codigoAcceso;
+    }
+}
